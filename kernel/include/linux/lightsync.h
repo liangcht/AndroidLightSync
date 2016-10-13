590bb0e317bdd949fbac01a4665442b3ad6a9723
liangcht <tj474474@gmail.com>
2016-10-13 18:17:34 -0400

    Complete light_evt_create

--- kernel/include/linux/lightsync.h
+++ kernel/include/linux/lightsync.h
@@ -3,14 +3,25 @@
 /*
 Use this wrapper to pass the intensity to your system call
 */
-  
-struct light_intensity{
+#define NOISE 10
+#define WINDOW 20
+
+struct light_intensity {
         int cur_intensity;
 };
   
 struct event_requirements {
         int req_intensity;
         int frequency;
+};
+
+struct light_event {
+	int req_intensity;
+	int frequency;
+	int ref_count;
+	int event_id;
+	wait_queue_head_t wq;
+	struct list_head event_list_head;
 };
   
 #endif
--- kernel/kernel/lightsync.c
+++ kernel/kernel/lightsync.c
@@ -1,7 +1,13 @@
 #include <linux/syscalls.h>
 #include <linux/lightsync.h>
+#include <linux/spinlock_types.h>
+#include <linux/slab.h>
 
 static struct light_intensity last_light_intensity;
+static LIST_HEAD(events);
+static DEFINE_SPINLOCK(events_lock);
+static int id_count = 0;
+
 SYSCALL_DEFINE1(set_light_intensity, 
 		struct light_intensity __user *, user_light_intensity)
 {
@@ -9,7 +15,7 @@ SYSCALL_DEFINE1(set_light_intensity,
 		 &(user_light_intensity->cur_intensity)) != 0) {
 		return -EFAULT;
 	}
-	printk("Current light intensity %d", 
+	printk("Current light intensity %d\n", 
 	       last_light_intensity.cur_intensity);
 	return 0;
 }
@@ -27,7 +33,31 @@ SYSCALL_DEFINE1(get_light_intensity,
 SYSCALL_DEFINE1(light_evt_create, 
 		struct event_requirements __user *, intensity_params)
 {
+	struct light_event *new_event;
+	new_event = (struct light_event *)
+		kmalloc(sizeof(struct light_event), GFP_KERNEL); 
+	
+	if (new_event == NULL) 
+		return -ENOMEM;
 
+	init_waitqueue_head(&new_event->wq);
+	INIT_LIST_HEAD(&new_event->event_list_head);
+	new_event->ref_count = 0;
+	
+	if (get_user(new_event->req_intensity, 
+		     &intensity_params->req_intensity) != 0) 
+		return -EFAULT;
+
+	if (get_user(new_event->frequency, &intensity_params->frequency) != 0) 
+		return -EFAULT;
+
+	new_event->frequency = 
+		(new_event->frequency > WINDOW) ? WINDOW : new_event->frequency;
+	spin_lock(&events_lock);	
+	new_event->event_id = id_count++;
+	list_add(&new_event->event_list_head, &events);
+	spin_unlock(&events_lock);
+	return 0;
 }
 
 SYSCALL_DEFINE1(light_evt_wait, int, event_id)
