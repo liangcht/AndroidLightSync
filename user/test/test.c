69872a81ea5eed2839984974a474007c574f28a9
Jin Fang <jf2966@columbia.edu>
2016-10-13 22:43:10 -0400

    add three children to test

--- user/test/test.c
+++ user/test/test.c
@@ -3,6 +3,9 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <sys/syscall.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 #include "light.h"
 
 int main (void)
@@ -20,14 +23,38 @@ int main (void)
 	high.req_intensity = 10000;
 	high.frequency = 10;
 
-	int evt1;
-	int evt2;
-	int evt3;
+	int n = 3;
+	int evt[3];	
+	evt[0] = syscall(__NR_light_evt_create, &low);
+	evt[1] = syscall(__NR_light_evt_create, &medium);
+	evt[2] = syscall(__NR_light_evt_create, &high);
+	//printf("%d,\n %d,\n %d\n", evt[0], evt[1], evt3);
 	
-	evt1 = syscall(__NR_light_evt_create, &low);
-	evt2 = syscall(__NR_light_evt_create, &medium);
-	evt3 = syscall(__NR_light_evt_create, &high);
-	printf("%d,\n %d,\n %d", evt1, evt2, evt3);
+	pid_t pid[3];
+	int i;
+	for (i = 0; i < n; i++) {
+		
+		pid[i] = fork();
+
+
+		if (pid[i] < 0) {
+
+		} else if (pid[i] == 0) {
+			if (syscall(__NR_light_evt_wait, evt[i]) == 0) {
+				printf("%d, WAIT SUCCESS!\n", i);
+			} else {
+				printf(strerror(errno));
+			}
+			exit(0);
+		}
+	}
+	
+	int status;
+	pid_t w_pid;
+	do {
+		w_pid = wait(&status);
+		n--;
+	} while (n > 0 && !WIFEXITED(status) && !WIFSIGNALED(status));
 
 	return 0;
 }
